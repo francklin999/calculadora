bf6631a63bcae700a5eb334f1b330898850ab0f1
Treinamento <Treinamento@RD723910.raiadrogasil.com>
2020-02-07 09:47:15 -0300

    quase pronta

--- js/logica.js
+++ js/logica.js
@@ -1,2 +1,12 @@
-let numero = document.querySelectorAll('#numeros')
+let numero = document.querySelectorAll('#numero7')
 let operador = document.querySelectorAll('#operadores')
+let clear = document.querySelector('#clear')
+
+clear.onclick = () =>{
+    document.getElementsByClassName('visor').value = '';
+}
+
+numero.onclick = () => {
+    document.getElementById("visor").textContent += numero.ele;
+}
+console.log(numero);
